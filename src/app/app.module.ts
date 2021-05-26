434f3fd70198c38ba39e92dd3f8ce4f297d15459
zpremium <zdravkoms99@gmail.com>
2021-05-26 18:41:18 +0200

    commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http'
+
+import { AppComponent } from './app.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
